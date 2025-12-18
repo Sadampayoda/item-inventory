caf26464fc6b7f67f9b053a3cfeda2f3a1010d6c
si prik tapi suka anin <111823958+Sadampayoda@users.noreply.github.com>
2025-12-18 20:27:54 +0700

    feat(db-001): set up db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('image')->nullable();
+            $table->enum('level',['admin','warehouse','manajement_warehouse']);
             $table->rememberToken();
             $table->timestamps();
         });
